b8995b40ddb27013456953f99501d5580c909bfa
julietteboyer <jboyer.sh@mysummitps.org>
2016-01-07 23:25:23 +0000

    update

--- calc.js
+++ calc.js
@@ -11,31 +11,43 @@ function doMath() {
     if  (dropdown.value == "+"){
         answer = parseInt(input1.value) + parseInt(input2.value);
         display.innerHTML = answer;
+        
+        //This if statement makes it so that if the "+" is selected from the dropdown, it will calculate the sum of whatever numbers are put into the two different spaces.
     }    
     
     if  (dropdown.value == "-"){
         answer = parseInt(input1.value) - parseInt(input2.value);
         display.innerHTML = answer;
+        
+        //This if statement makes it so that if the "-" is selected from the dropdown, it will calculate the difference between whatever numbers are placed in the two different spaces.
     }
     
     if  (dropdown.value == "x"){
         answer = parseInt(input1.value) * parseInt(input2.value);
         display.innerHTML = answer;
+        
+        //This if statement makes it so that if the "*" is selected from the dropdown, it will calculate the product of whatever numbers are placed in the two different spaces.
     }
     
     if  (dropdown.value == "/"){
         answer = parseInt(input1.value) / parseInt(input2.value);
         display.innerHTML = answer;
+        
+        //This if statement makes it so that if the "/" is selected from the dropdown, it will divide the number in the first space by the number in the second space.
     }
     
     if (dropdown.value == "^ (to the power of)"){
         answer = Math.pow(parseInt(input1.value) , parseInt(input2.value));
         display.innerHTML = answer;
+        
+        //This if statement makes it so that if the "^ (to the power of)" is selected from the dropdown, it will calculate the number in the first space to the power of the number in the second space.
     } 
     
     if  (dropdown.value == ", (square root)"){
         answer = Math.pow(parseInt(input1.value) , 1/parseInt(input2.value));
         display.innerHTML = answer
+        
+        //This if statement makes it so that if the ", (square root)" is selected from the dropdown, it will calculate the square root of the first number, if the second number is 2.
     }
     
     
--- info.js
+++ info.js
@@ -4,8 +4,8 @@ var info1 = document.getElementById("info1");
 var info2 = document.getElementById("info2");
 
 item1.addEventListener("click", function(){
-    info1.innerHTML = "Some stuff";
-    info2.innerHTML = "some other stuff";
+    info1.innerHTML = "<p style='margin-left:35%; id='info' > One Direction, a British-Irish pop boy band, were formed on July 23, 2010, on the British television series, The X Factor. Each of the members, Harry Styles, Liam Payne, Niall Horan, Louis Tomlinson, and Zayn Malik, auditioned for the show individually but none of them qualified to the next segment of the show, the judge's houses. However, head judge, Simon Cowell, thought these boys were too talented to give up and put them into a group so that they could go to the judge's houses under the band category. Styles came up with the name One Direction and the band made it all the way to the finals, where they placed third. Despite their placement, Simon Cowell still signed them to his label. They had gained hundreds of extremely dedicated fans in the UK and across Europe while on the show. These fans quickly spread the word about One Direction, and they were soon one of the most talked about bands all over the world. Since 2010, One Direction have made 5 albums, gone on 5 world tours, and won countless awards.</p>";
+    info2.innerHTML = "<p style='margin-left:20% margin-top:40px' id='info2' >  </p>";
 });
                        
 item2.addEventListener("click", function(){
