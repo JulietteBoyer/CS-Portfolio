e62b53fe9a7fb64ae4105aedcd819c060d3d08d2
julietteboyer <jboyer.sh@mysummitps.org>
2016-03-28 22:24:06 +0000

    update

---
+++ lifecycleoffrog.js
@@ -0,0 +1,7 @@
+var nextbutton = document.getElementById("next button");
+var adultfrog = document.getElementById("adult frog");
+var eggs = document.getElementById("eggs");
+
+nextbutton.addEventListener("click", function(){
+    eggs.src = "http://content.mycutegraphics.com/graphics/frog/frog-eggs-thumb.gif";
+});
