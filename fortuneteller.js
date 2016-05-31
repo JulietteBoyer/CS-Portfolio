3b8c3b8288e1eb8ad8c0946efdde2a3003d7468e
julietteboyer <jboyer.sh@mysummitps.org>
2016-05-31 21:44:44 +0000

    update

---
+++ fortuneteller.js
@@ -0,0 +1,8 @@
+var item1 = document.getElementById("myFortune");
+var info1 = document.getElementById("info1");
+
+item1.addEventListener("click", function(){
+    info1.innerHTML = "<p style='margin-left:50%; id='myFortune' > You will overcome adversity and furthermore live a successful, productive life full of satisfaction!</p>";
+    
+});
+                      
